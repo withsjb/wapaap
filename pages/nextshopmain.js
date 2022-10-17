54f669b310cd056e2eaf85f1c7edbd17306bc6e8
“withsjb” <withsjb1@gmail.com>
2022-10-18 02:47:46 +0900

    frofilecomplite

---
+++ pages/nextshopmain.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../components/Layout";
+import ProductItem from "../components/Productitem";
+import styles from "../styles/Home.module.css";
+import data from "../utils/data";
+
+export default function Home() {
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
+}
