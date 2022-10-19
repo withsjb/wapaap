ba2b99b0e929234f599fa3cb560ea7688e70119a
“withsjb” <withsjb1@gmail.com>
2022-10-19 18:32:12 +0900

    error1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "../styles/frofile.module.css";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function frofile() {
   return (
