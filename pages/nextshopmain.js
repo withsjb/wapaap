c1f046739466cf554b94c7991185f3ec24fc61e8
“withsjb” <withsjb1@gmail.com>
2022-10-19 18:32:23 +0900

    error2

--- pages/nextshopmain.js
+++ pages/nextshopmain.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import Layout from "../components/Layout";
 import ProductItem from "../components/Productitem";
 import styles from "../styles/Home.module.css";
